edc4233c017c4067e6e03c02d5a1a0f786c29210
LostInCompilation <12819635+LostInCompilation@users.noreply.github.com>
2024-06-10 06:07:16 +0200

    Initial commit

---
+++ ReedSolomonEC/GaloisField.cpp
@@ -0,0 +1,8 @@
+//
+//  GaloisField.cpp
+//  ReedSolomonEC
+//
+//  Created by Marc Schöndorf on 09.06.24.
+//
+
+#include "GaloisField.hpp"
---
+++ ReedSolomonEC/GaloisField.hpp
@@ -0,0 +1,19 @@
+//
+//  GaloisField.hpp
+//  ReedSolomonEC
+//
+//  Created by Marc Schöndorf on 09.06.24.
+//
+
+#ifndef GaloisField_hpp
+#define GaloisField_hpp
+
+class GaloisField_
+{
+private:
+    
+public:
+    
+};
+
+#endif /* GaloisField_hpp */
