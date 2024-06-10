f2148f6350749cf154d2fef453b347542cd316f5
LostInCompilation <12819635+LostInCompilation@users.noreply.github.com>
2024-06-10 17:57:06 +0200

    Cleanup

---
+++ ReedSolomonEC/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  ReedSolomonEC
+//
+//  Created by Marc Schöndorf on 09.06.24.
+//
+
+#include <iostream>
+
+#include "GaloisField.hpp"
+
+int main()
+{
+    std::cout << "Reed-Solomon error correction" << std::endl;
+    
+    return 0;
+}
