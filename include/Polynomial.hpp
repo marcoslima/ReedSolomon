ab18e17dd96159e3d104fcb26fa7244d4c0824f6
Marcos Lima <marcos.lima@stormgroup.com.br>
2025-04-06 23:59:07 -0300

    refactor: several CTidy suggestions applied and use of reverse_view + iota in CalculateErrorLocatorPolynomial.

--- include/Polynomial.hpp
+++ include/Polynomial.hpp
@@ -36,50 +36,50 @@ the following restrictions:
 
 namespace NReedSolomon
 {
+    using coef_diff_type = std::vector<RSWord>::difference_type;
 class Polynomial
 {
-private:
     const GaloisField*  m_GaloisField = nullptr;
     
     uint64_t            m_NumOfCoefficients = 0;
     std::vector<RSWord> m_Coefficients;
     
 public:
-    explicit Polynomial(const GaloisField* const galoisField);
-    Polynomial(const std::vector<RSWord>& coefficients, const GaloisField* const galoisField);
-    Polynomial(const RSWord* const coefficients, const uint64_t numOfCoefficients, const GaloisField* const galoisField);
+    explicit Polynomial(const GaloisField*galoisField);
+    Polynomial(const std::vector<RSWord>& coefficients, const GaloisField*galoisField);
+    Polynomial(const RSWord*coefficients, uint64_t numOfCoefficients, const GaloisField*galoisField);
     
-    void SetNew(const std::vector<RSWord>& coefficients, const GaloisField* const galoisField = nullptr);
-    void SetNew(const RSWord* const coefficients, const uint64_t numOfCoefficients, const GaloisField* const galoisField = nullptr);
+    void SetNew(const std::vector<RSWord>& coefficients, const GaloisField*galoisField = nullptr);
+    void SetNew(const RSWord*coefficients, uint64_t numOfCoefficients, const GaloisField*galoisField = nullptr);
     
     void Clear();
-    void Print(const std::string& name = "Polynomial", const bool printAsHexadecimal = true, const bool printSize = true) const;
-    void PrintAsASCIICharacters(const std::string& name = "Polynomial", const bool printSize = true) const;
+    void Print(const std::string& name = "Polynomial", bool printAsHexadecimal = true, bool printSize = true) const;
+    void PrintAsASCIICharacters(const std::string& name = "Polynomial", bool printSize = true) const;
     
     // Operations changing object state
-    void Add(const Polynomial* const polynomial);
-    void Scale(const RSWord scalar);
-    void Multiply(const Polynomial* const polynomial);
-    void Divide(const Polynomial* const divisor, Polynomial* const quotient_out = nullptr, Polynomial* const remainder_out = nullptr);
+    void Add(const Polynomial*polynomial);
+    void Scale(RSWord scalar);
+    void Multiply(const Polynomial*polynomial);
+    void Divide(const Polynomial*divisor, Polynomial*quotient_out = nullptr, Polynomial*remainder_out = nullptr);
     void Reverse();
     
     // Operations not changing object state
-    Polynomial operator* (const RSWord scalar) const;
-    RSWord Evaluate(const RSWord x) const;
-    std::vector<uint64_t> ChienSearch(const uint64_t max) const;
+    Polynomial operator* (RSWord scalar) const;
+    [[nodiscard]] RSWord Evaluate(RSWord x) const;
+    [[nodiscard]] std::vector<uint64_t> ChienSearch(uint64_t max) const;
     
     // Change size
-    void Enlarge(const uint64_t numElementsToAdd, const RSWord value = 0);
-    void TrimEnd(const uint64_t numElementsToTrim);
-    void TrimBeginning(const uint64_t numElementsToTrim);
+    void Enlarge(uint64_t numElementsToAdd, RSWord value = 0);
+    void TrimEnd(uint64_t numElementsToTrim);
+    void TrimBeginning(uint64_t numElementsToTrim);
     
     // Getter
-    uint64_t GetNumberOfCoefficients() const { return m_NumOfCoefficients; }
-    const std::vector<RSWord>* GetCoefficients() const noexcept { return &m_Coefficients; }
+    [[nodiscard]] uint64_t GetNumberOfCoefficients() const { return m_NumOfCoefficients; }
+    [[nodiscard]] const std::vector<RSWord>* GetCoefficients() const noexcept { return &m_Coefficients; }
     
-    RSWord operator[] (uint64_t index) const { return m_Coefficients[index]; }
-    RSWord& operator[] (uint64_t index) { return m_Coefficients[index]; }
+    RSWord operator[] (const uint64_t index) const { return m_Coefficients[index]; }
+    RSWord& operator[] (const uint64_t index) { return m_Coefficients[index]; }
 };
-};
+}
 
 #endif /* Polynomial_hpp */
